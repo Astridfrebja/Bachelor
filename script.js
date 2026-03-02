47f5ec3a148831f808e99e6ec6c64800bfa5989f
h678159 <astridfrebja@gmail.com>
2026-03-02 14:10:07 +0100

    Mye fiks

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
     hamburger.addEventListener("click", function (e) {
         e.stopPropagation();
         dropdown.classList.toggle("active");
+        hamburger.classList.toggle("active");
+    });
+
+    dropdown.addEventListener("click", function (e) {
+        e.stopPropagation();
     });
 
     document.addEventListener("click", function () {
         dropdown.classList.remove("active");
+        hamburger.classList.remove("active");
     });
 
 });
